cf7d326d403a87c29710720151c0c8ced66b2933
Indra Kumar R <indra07kumar@gmail.com>
2025-11-09 18:57:06 +0530

    feat: implement user service

--- scrapy-frontend/src/app/core/services/backend/user.service.ts
+++ scrapy-frontend/src/app/core/services/backend/user.service.ts
@@ -1,8 +1,45 @@
 import { Injectable } from '@angular/core';
+import { HttpService } from '../http/http.service';
+import { environment } from '../../../../environments/environments';
+import { Observable } from 'rxjs';
+import { Users, User } from '../../../models/user.model';
+import { UsersApis } from '../../constants/api-routes-constants';
 
 @Injectable({
   providedIn: 'root',
 })
 export class UserService {
-  constructor() {}
+  private readonly apiUrl = environment.api.user;
+
+  constructor(private http: HttpService) {}
+
+  getUsers(): Observable<Users> {
+    return this.http.get<Users>(UsersApis.USERS, this.apiUrl);
+  }
+
+  create(user: User): Observable<User> {
+    return this.http.post<User>(UsersApis.USERS, user, this.apiUrl);
+  }
+
+  get(userId: string): Observable<User> {
+    return this.http.get<User>(
+      `${UsersApis.USERS}/${encodeURIComponent(userId)}`,
+      this.apiUrl
+    );
+  }
+
+  update(userId: string, user: Partial<User>): Observable<User> {
+    return this.http.put<User>(
+      `${UsersApis.USERS}/${encodeURIComponent(userId)}`,
+      user,
+      this.apiUrl
+    );
+  }
+
+  delete(userId: string): Observable<void> {
+    return this.http.delete<void>(
+      `${UsersApis.USERS}/${encodeURIComponent(userId)}`,
+      this.apiUrl
+    );
+  }
 }
