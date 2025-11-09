9588ca9122d11e8d92f6e275f536a1b50eb127a6
INDRA KUMAR R <indra@geektrust.in>
2025-11-09 18:39:00 +0530

    feat: configure application routes and add authentication guards and services

--- scrapy-frontend/src/app/app.routes.ts
+++ scrapy-frontend/src/app/app.routes.ts
@@ -1,3 +1,71 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './features/login/login.component';
+import { HomeComponent } from './features/home/home.component';
+import { UserComponent } from './features/user/user.component';
+import { HistoryComponent } from './features/history/history.component';
+import { SearchQueriesComponent } from './features/history/search-queries/search-queries.component';
+import { SearchQueryComponent } from './features/history/search-query/search-query.component';
+import { SearchComponent } from './features/search/search.component';
+import { PageNotFoundComponent } from './features/page-not-found/page-not-found.component';
+import { authGuard, loginGuard } from './core/services/auth';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full',
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+    canActivate: [loginGuard],
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
+    canActivate: [authGuard],
+  },
+  {
+    path: 'user',
+    component: UserComponent,
+    canActivate: [authGuard],
+  },
+  {
+    path: 'history',
+    component: HistoryComponent,
+    canActivate: [authGuard],
+    children: [
+      {
+        path: '',
+        redirectTo: 'search-queries',
+        pathMatch: 'full',
+      },
+      {
+        path: 'search-queries',
+        component: SearchQueriesComponent,
+      },
+      {
+        path: 'search-queries/:searchId',
+        component: SearchQueryComponent,
+      },
+    ],
+  },
+  {
+    path: 'search',
+    canActivate: [authGuard],
+    children: [
+      {
+        path: '',
+        component: SearchComponent, // default search
+      },
+      {
+        path: ':searchId',
+        component: SearchComponent, // same component, different param
+      },
+    ],
+  },
+  {
+    path: '**',
+    component: PageNotFoundComponent,
+  },
+];
