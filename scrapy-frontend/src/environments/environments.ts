821fff96db8e327df26e1c0d3784210850016849
Indra Kumar R <indra07kumar@gmail.com>
2025-11-09 18:56:44 +0530

    feat: implement http service for api calls and update environments

--- scrapy-frontend/src/environments/environments.ts
+++ scrapy-frontend/src/environments/environments.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: false,
   api: {
-    user: 'http://localhost:8081/api/v1',
-    search: 'http://localhost:8080/api/v1',
-    storage: 'http://localhost:8082/api/v1',
+    user: 'http://localhost:8081',
+    search: 'http://localhost:8080',
+    storage: 'http://localhost:8082',
   },
 };
