d450c9bc752aa967e44906639e9406b5634fbb13
Indra Kumar R <indra07kumar@gmail.com>
2025-11-11 12:41:14 +0530

    feat: implement search queries history feature with details and copy action

--- scrapy-frontend/src/app/features/history/search-queries/search-queries.component.ts
+++ scrapy-frontend/src/app/features/history/search-queries/search-queries.component.ts
@@ -1,6 +1,17 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { SidebarComponent } from '../../../shared/sidebar/sidebar.component';
+import { catchError, finalize, of, Subject, takeUntil, tap } from 'rxjs';
+import { User } from '../../../models/user.model';
+import { StorageService } from '../../../core/services/backend';
+import { ToasterService } from '../../../core/services/toaster/toaster.service';
+import { BrowserStorageService } from '../../../core/services/browser-storage/browser-storage.service';
+import {
+  GroupedSearchResults,
+  UserSearchResultsParams,
+  UserSearchResultsResponse,
+} from '../../../models/storage.model';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-search-queries',
@@ -8,4 +19,70 @@ import { SidebarComponent } from '../../../shared/sidebar/sidebar.component';
   templateUrl: './search-queries.component.html',
   styleUrl: './search-queries.component.scss',
 })
-export class SearchQueriesComponent {}
+export class SearchQueriesComponent implements OnInit, OnDestroy {
+  private destroy$ = new Subject<void>();
+
+  userId!: string;
+  user!: User;
+
+  loadingSearchQueries = false;
+  page = 1;
+  pageSize = 10;
+
+  searchResults: GroupedSearchResults[] = [];
+
+  constructor(
+    private storageService: StorageService,
+    private browserStorageService: BrowserStorageService,
+    private router: Router,
+    private toasterService: ToasterService
+  ) {}
+
+  ngOnInit(): void {
+    this.userId = this.browserStorageService?.auth?.userId!;
+    this.fetchUserSearchQueries();
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
+
+  fetchUserSearchQueries(): void {
+    this.loadingSearchQueries = true;
+
+    const params: UserSearchResultsParams = {
+      userId: this.userId,
+      page: this.page,
+      pageSize: this.pageSize,
+    };
+
+    this.storageService
+      .getUserSearchResults(params)
+      .pipe(
+        tap((res: UserSearchResultsResponse) => {
+          this.searchResults = res?.results ?? [];
+        }),
+        catchError((err) => {
+          console.error('Error: ', err);
+          this.searchResults = [];
+          this.toasterService.toast('Error fetching user search queries');
+          return of([]);
+        }),
+        finalize(() => {
+          this.loadingSearchQueries = false;
+        }),
+        takeUntil(this.destroy$)
+      )
+      .subscribe();
+  }
+
+  viewSearchResults(searchId: string): void {
+    this.router.navigate(['/history/search-queries', searchId]);
+  }
+
+  copySearchQuery(searchQuery: string): void {
+    navigator.clipboard.writeText(searchQuery);
+    this.toasterService.toast('Search query copied to clipboard');
+  }
+}
